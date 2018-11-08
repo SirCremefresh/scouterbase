96386de1bf057973a0a692a455106864a2d07e64
Donato Wolfisberg <donato.wolfisberg@gmail.com>
2018-11-08 17:47:12 +0100

    Added some swag

--- scouterbase-frontend/src/app/app.component.ts
+++ scouterbase-frontend/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'scouterbase-frontend';
+  title = 'scouterbase-frontend SWAG';
 }
